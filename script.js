acc2f3b1cdaa482ebe4ccbe0bad11b1e9d2384fd
Fiqriananda <fiqrianandahakin@gmail.com>
2022-06-08 00:58:12 +0800

    add javascript to direct link and some style

---
+++ script.js
@@ -0,0 +1,18 @@
+function directlinkinstagram(){
+
+    window.open('https://instagram.com');
+
+}
+
+function directlinkfacebook(){
+
+    window.open('https://facebook.com');
+
+}
+
+
+function directlinkdiscord(){
+
+    window.open('https://discord.com');
+
+}
